31f84a3c9b70decb599b78f17c986bc718da32b3
Akacia <akaciapark@gmail.com>
2022-10-25 18:53:29 -0300

    passed on toast test

--- app/src/main/java/com/example/myfirstapp/FirstFragment.kt
+++ app/src/main/java/com/example/myfirstapp/FirstFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.myfirstapp.databinding.FragmentFirstBinding
 
@@ -34,6 +36,14 @@ class FirstFragment : Fragment() {
 
         binding.randomButton.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+        view.findViewById<Button>(R.id.random_button).setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+
+        view.findViewById<Button>(R.id.toast_button).setOnClickListener {
+            val myToast = Toast.makeText(context, "Hello Toast", Toast.LENGTH_SHORT)
+            myToast.show()
         }
     }
 
